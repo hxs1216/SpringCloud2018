2daf0d7f4894b3c8a47bcd25995ba08339a3aed9
hxs <394480037@qq.com>
2019-05-26 16:49:40 +0800

    zuul 路由网关

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/biyao/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,19 @@
+package com.biyao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Author:hxs
+ * @Description:zuul路由启动器
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_StartSpringCloudApp
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
+	}
+}
